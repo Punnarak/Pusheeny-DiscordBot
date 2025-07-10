70af7a8037b8643cfc0b1927045e345ce481ba00
D4N-hub <kannidhis@gmail.com>
2025-07-10 14:27:43 +0000

    edit emoji stored in info_dict

--- actions/common_action_util.py
+++ actions/common_action_util.py
@@ -1,10 +1,12 @@
 import os
 
 info_dict = {
-  #  'key' : ['file', 'title', [strings for use in messages]]
-    'food' : ["food_list.txt", "📋 รายการอาหาร", ["เมนู", "🍽️"]],
-    'movie' : ["movie_list.txt", "🍿 รายการหนัง", ["เรื่อง", "📽️"]]
+    #  'key' : ['file', 'title', [strings for use in messages]]
+    'food': ["food_list.txt", "📋 รายการอาหาร", ["เมนู", "🍽️"]],
+    'movie': ["movie_list.txt", "🍿 รายการหนัง", ["เรื่อง", "📽️"]],
+    'restaurant': ["restaurant_list.txt", "🍿 รายการร้านอาหาร", ["ร้าน", "📽️"]]
 }
+
 
 def read_item_list(listFile):
   if not os.path.exists(listFile):
@@ -12,6 +14,7 @@ def read_item_list(listFile):
   with open(listFile, "r", encoding="utf-8") as f:
     return [line.strip() for line in f if line.strip()]
 
-def save_item_list(file,itemList):
+
+def save_item_list(file, itemList):
   with open(file, "w", encoding="utf-8") as f:
-      f.write("\n".join(itemList) + "\n")
+    f.write("\n".join(itemList) + "\n")
