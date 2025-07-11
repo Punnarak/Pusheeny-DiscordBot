f73ddb820974750ab3003ea2ca1e72c527de8437
D4N-hub <kannidhis@gmail.com>
2025-07-11 05:56:18 +0000

    add common util file for pokemon category

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -1,11 +1,10 @@
 import pkgutil
 
-
 async def setup(bot):
     package = __name__
     for _, module_name, is_pkg in pkgutil.iter_modules(__path__):
         if not is_pkg:
-            if (module_name != "pokemon_storage"):
+            if module_name not in ["pokemon_storage", "common_pokemon_util"]:
                 module = f"{package}.{module_name}"
                 await bot.load_extension(module)
                 print(f"✅ Loaded actions extension: {module}")
---
+++ pokemon/common_pokemon_util.py
@@ -0,0 +1,102 @@
+import os
+import json
+import aiohttp
+import random
+
+POKEMON_STORAGE = "pokemon/pokemon_storage.json"
+
+
+def load_pokemon_data():
+    if os.path.exists(POKEMON_STORAGE):
+        with open(POKEMON_STORAGE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return {}
+
+def save_pokemon_data(data):
+    with open(POKEMON_STORAGE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def is_shiny():
+    return random.randint(1, 4096) == 1
+
+def catch_chance(cap_rate): # กำหนดโอกาสจับได้ตาม cap_rate
+    chances = ((cap_rate - 255) / 255) + 1
+    return chances
+
+def get_rarity_emoji(legendary, mythical):
+    emojis = {
+        "ธรรมดา (Common)": "⚪",
+        "Uncommon": "🟢",
+        "หายาก (Rare)": "🔵",
+        "Epic": "🟣",
+        "ตำนาน (Legendary)": "🟡",
+        "Mythical": "🌟",
+        "Ultra Beast": "🔥",
+        "other": "❓"
+    }
+    temp = ""
+    if legendary or mythical:
+        if legendary:
+            temp += emojis["ตำนาน (Legendary)"]
+        if mythical:
+            temp += emojis["Mythical"]
+    else:
+        temp = emojis["ธรรมดา (Common)"]
+    return temp
+
+async def fetch_pokemon_data_with_api(identifier, url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                return await resp.json()
+            else:
+                return None
+            
+
+async def fetch_pokemon_data():
+    pokemon_id = random.randint(1, 1025)    #random pokemon id
+    # fetch pokemon data part1
+    url = f"https://pokeapi.co/api/v2/pokemon/{str(pokemon_id)}"
+    data = await fetch_pokemon_data_with_api(str(pokemon_id), url)
+    if not data:
+        return None
+
+    shiny = is_shiny()
+    sprite = data['sprites']['front_shiny'] if shiny else data['sprites']['front_default']
+    stats = {
+      stat['stat']['name']: stat['base_stat']
+      for stat in data['stats']
+    }
+    pokemon = {
+        "name": data["name"].capitalize(),
+        "id": data["id"],
+        "types": ", ".join(t["type"]["name"] for t in data["types"]),
+        "sprite": sprite,
+        "shiny": shiny,
+        "stats": stats
+    }
+    # fetch pokemon data part2
+    url2 = f"https://pokeapi.co/api/v2/pokemon-species/{str(pokemon_id)}"
+    data2 = await fetch_pokemon_data_with_api(str(pokemon_id), url2)
+    if not data2:
+        return None
+    cap_rate = data2["capture_rate"]
+    pokemon["capture_rate"] = cap_rate
+    pokemon["is_legendary"] = data2["is_legendary"]
+    pokemon["is_mythical"] = data2["is_mythical"]
+    # check legendary or mythical
+    if data2["is_legendary"]:
+        leg = "โปเกม่อนในตำนาน"
+    else:
+        leg = ""
+    if data2["is_mythical"]:
+        myth = "โปเกม่อนเทพนิยาย"
+    else:
+        myth = ""
+    if data2["is_legendary"] or data2["is_mythical"]:
+        leg_or_myth = f"[{leg}{myth}]"
+    else:
+        leg_or_myth = ""
+    pokemon["leg_or_myth"] = leg_or_myth
+
+    return pokemon    # keys: name, id, types, sprite, shiny, stats, capture_rate, is_legendary, is_mythical, leg_or_myth
