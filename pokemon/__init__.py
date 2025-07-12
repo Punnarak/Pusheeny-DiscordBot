f9b4ccc6d5ed666a97f8271964c32a7d3f454f33
D4N-hub <kannidhis@gmail.com>
2025-07-12 22:30:33 +0700

    update module ignore in extension loading

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -5,7 +5,7 @@ async def setup(bot):
     package = __name__
     for _, module_name, is_pkg in pkgutil.iter_modules(__path__):
         if not is_pkg:
-            if module_name not in ["pokemon_storage", "common_pokemon_util"]:
+            if module_name not in ["pokemon_storage", "common_pokemon_util", "pagination_pokemon"]:
                 module = f"{package}.{module_name}"
                 await bot.load_extension(module)
                 print(f"✅ Loaded actions extension: {module}")
