43be576c373516eface57a603c297fad4ff20f05
D4N-hub <kannidhis@gmail.com>
2025-07-13 01:00:43 +0700

    add paged search function

---
+++ pokemon/search_pokemon.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+from . import common_pokemon_util as cpu
+from . import search_result_view as srv
+
+
+class SearchPokemon(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(name="searchpokemon", help="search from your pokemon list")
+    async def searchpokemon(self, ctx, *, keyword: str):
+        keyword_lower = keyword.lower()
+        data = cpu.load_pokemon_data()
+        all_pokemon = data.get(str(ctx.author.id), [])
+
+        if not all_pokemon:
+            await ctx.send("คุณยังไม่มีโปเกมอนเลยนะ!")
+            return
+
+        results = [p for p in all_pokemon if keyword_lower in p["name"].lower()]
+
+        if not results:
+            await ctx.send(f"ไม่พบโปเกมอนที่มีคำว่า \"{keyword}\"")
+            return
+
+        view = srv.SearchResultsView(ctx, results, keyword)
+        embed = view.generate_embed()
+        message = await ctx.send(embed=embed, view=view)
+        view.message = message
+
+async def setup(bot):
+    await bot.add_cog(SearchPokemon(bot))
